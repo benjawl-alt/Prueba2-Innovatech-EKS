e1d1e7c2ea1c32a5598fb0326215df4a1364eec3
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-18 11:41:13 -0400

    fix: actualizar dato en vivoddd

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra 💰 prueba de q funciona"
+          title="Consultar Ordenes de compra 💰 ooooo"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
