98c2c91943b30db0896c0b5b83364d92d755dfcf
benjawl-alt <benj.vasquezc@duocuc.cl>
2026-06-18 17:00:40 -0400

    Prueba va o no

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra 💰 Prueba de q funciona :))) "
+          title="Consultar Ordenes de compra 💰 va "
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
